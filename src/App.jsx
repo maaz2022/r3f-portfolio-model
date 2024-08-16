6483366412b4662a3125586c5a817eb3791dab68
maaz2022 <maaznazeer098@gmail.com>
2024-08-16 18:09:19 +0500

    Menu added

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import { Scroll, ScrollControls } from "@react-three/drei";
 import { Interface } from "./components/Interface";
 import { useState } from "react";
 import { ScrollManager } from "./components/ScrollManager";
+import { Menu } from "./components/Menu";
+
 
 function App() {
   const [section, setSection] = useState(0);
+  const [menuOpened, setMenuOpened] = useState(false);
   return (
 <>
     <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
@@ -19,7 +22,9 @@ function App() {
       </Scroll>
       </ScrollControls>
     </Canvas>
+        <Menu  onSectionChange={setSection} menuOpened={menuOpened} setMenuOpened={setMenuOpened}/>
 
+  
 </>
 
   );

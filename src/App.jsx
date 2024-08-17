110c457747cc94d3ec88a1273b694841c6b6a093
maaz2022 <maaznazeer098@gmail.com>
2024-08-17 13:07:16 +0500

    Office model loaded

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,41 @@ import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
 import { Scroll, ScrollControls } from "@react-three/drei";
 import { Interface } from "./components/Interface";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ScrollManager } from "./components/ScrollManager";
 import { Menu } from "./components/Menu";
+import { MotionConfig } from "framer-motion";
 
 
 function App() {
   const [section, setSection] = useState(0);
   const [menuOpened, setMenuOpened] = useState(false);
+  useEffect(() => {
+    setMenuOpened(false)
+  },[section]);
   return (
 <>
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
+    <MotionConfig
+    transition={{
+      type:"spring",
+      stiffness: 500,
+      damping: 50,
+      mass: 5,  
+      restDelta:0.0001,
+    }}
+    >
+      <Canvas shadows camera={{ position: [0, 3, 10], fov: 42 }}>
+          <color attach="background" args={["#e6e7ff"]} />
       <ScrollControls pages={4} damping={0.1}>
-      <ScrollManager section={section} onSectionChange={setSection} />
-      <Experience />
-      <Scroll html>
+        <ScrollManager section={section} onSectionChange={setSection} />
+        <Experience section={section}/>
+          <Scroll html>
           <Interface />
       </Scroll>
       </ScrollControls>
     </Canvas>
         <Menu  onSectionChange={setSection} menuOpened={menuOpened} setMenuOpened={setMenuOpened}/>
-
-  
+    </MotionConfig>
 </>
 
   );
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,23 @@
-import { OrbitControls } from "@react-three/drei";
 
-export const Experience = () => {
+import { Office } from "./Office";
+import {motion} from "framer-motion-3d"
+
+
+export const Experience = (props) => {
+   const {section} = props;
   return (
     <>
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+    <ambientLight intensity={1}/>
+    <motion.group 
+    position={[1.5,2,3]} 
+    scale={[0.9,0.9,0.9]} 
+    rotation-y={-Math.PI / 4}
+    animate={{
+      y: section ? 0 : -1
+    }}
+    >
+       <Office/>
+    </motion.group>
     </>
   );
 };

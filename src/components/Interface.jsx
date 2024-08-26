75dcae68b531503ba4d0629ffc27dd36d570921d
maaz2022 <maaznazeer098@gmail.com>
2024-08-26 17:50:10 +0500

    skill section color change to white

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -111,94 +111,91 @@ const languages = [
 
 const SkillsSection = () =>{
     return(
-<Section>
-    <motion.div whileInView={'visible'}>
-        <h2 className="text-5xl font-bold">
-            Skills
-        </h2>
-        <div className="mt-8 space-y-4">
-            {Skills.map((skill, index) => (
-                <div className="w-64" key={index}>
-                    <motion.h3 className="text-xl font-bold text-gray-800"
-                    initial={{
-                        opacity: 0
-                    }}
-                    variants={{
-                        visible:{
-                            opacity: 1,
-                        transition:{
-                        duration: 1,
-                        delay: 1 + index * 0.2
-                    }}
-                    }} >
-                        {skill.title}
-                    </motion.h3>
-                    <div className="h-2 w-full bg-white rounded-full mt-2">
-                        <motion.div
-                            className="h-full bg-indigo-500 rounded-full"
-                            style={{ width: `${skill.level}%` }}
-                            initial={{
-                            scaleX:0,
-                            originX:0
-                            }}
-                    variants={{
-                        visible:{
-                            scaleX: 1,
-                        transition:{
-                        duration: 1,
-                        delay: 1 + index * 0.2
-                    }}
-                    }} 
-                    />
-                    </div>
+        <Section>
+            <motion.div whileInView={'visible'}>
+                <h2 className="text-5xl font-bold text-white">
+                    Skills
+                </h2>
+                <div className="mt-8 space-y-4 text-white">
+                    {Skills.map((skill, index) => (
+                        <div className="w-64" key={index}>
+                            <motion.h3 className="text-xl font-bold text-white" // Change to text-white
+                                initial={{ opacity: 0 }}
+                                variants={{
+                                    visible: {
+                                        opacity: 1,
+                                        transition: {
+                                            duration: 1,
+                                            delay: 1 + index * 0.2
+                                        }
+                                    }
+                                }}
+                            >
+                                {skill.title}
+                            </motion.h3>
+                            <div className="h-2 w-full bg-white rounded-full mt-2">
+                                <motion.div
+                                    className="h-full bg-indigo-500 rounded-full"
+                                    style={{ width: `${skill.level}%` }}
+                                    initial={{ scaleX: 0, originX: 0 }}
+                                    variants={{
+                                        visible: {
+                                            scaleX: 1,
+                                            transition: {
+                                                duration: 1,
+                                                delay: 1 + index * 0.2
+                                            }
+                                        }
+                                    }}
+                                />
+                            </div>
+                        </div>
+                    ))}
                 </div>
-            ))}
-        </div>
-    </motion.div>
- <motion.div whileInView={'visible'}>
-      <h2 className="text-5xl font-bold mt-10">Languages</h2>
-        <div className="mt-8 space-y-4">
-            {languages.map((lng, index) => (
-                <div className="w-64" key={index}>
-                    <motion.h3 className="text-xl font-bold text-gray-800"
-                    initial={{
-                        opacity: 0
-                    }}
-                    variants={{
-                        visible:{
-                            opacity: 1,
-                        transition:{
-                        duration: 1,
-                        delay: 2 + index * 0.2
-                    }}
-                    }} >
-                        {lng.title}
-                    </motion.h3>
-                    <div className="h-2 w-full bg-white rounded-full mt-2">
-                        <motion.div
-                            className="h-full bg-indigo-500 rounded-full"
-                            style={{ width: `${lng.level}%` }}
-                            initial={{
-                            scaleX:0,
-                            originX:0
-                            }}
-                    variants={{
-                        visible:{
-                            scaleX: 1,
-                        transition:{
-                        duration: 1,
-                        delay: 2 + index * 0.2
-                    }}
-                    }} 
-                    />
-                    </div>
+            </motion.div>
+            <motion.div whileInView={'visible'}>
+                <h2 className="text-5xl font-bold mt-10 text-white">Languages</h2>
+                <div className="mt-8 space-y-4 text-white">
+                    {languages.map((lng, index) => (
+                        <div className="w-64" key={index}>
+                            <motion.h3 className="text-xl font-bold text-white" // Change to text-white
+                                initial={{ opacity: 0 }}
+                                variants={{
+                                    visible: {
+                                        opacity: 1,
+                                        transition: {
+                                            duration: 1,
+                                            delay: 2 + index * 0.2
+                                        }
+                                    }
+                                }}
+                            >
+                                {lng.title}
+                            </motion.h3>
+                            <div className="h-2 w-full bg-white rounded-full mt-2">
+                                <motion.div
+                                    className="h-full bg-indigo-500 rounded-full"
+                                    style={{ width: `${lng.level}%` }}
+                                    initial={{ scaleX: 0, originX: 0 }}
+                                    variants={{
+                                        visible: {
+                                            scaleX: 1,
+                                            transition: {
+                                                duration: 1,
+                                                delay: 2 + index * 0.2
+                                            }
+                                        }
+                                    }}
+                                />
+                            </div>
+                        </div>
+                    ))}
                 </div>
-            ))}
-        </div>
-    </motion.div>
-</Section>
+            </motion.div>
+        </Section>
     )
 }
+
 const ProjectsSection = () => {
   const [currentProject, setCurrentProject] = useAtom(currentProjectAtom);
 
